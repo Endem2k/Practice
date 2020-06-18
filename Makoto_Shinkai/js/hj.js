49a3a1d5835a0fd37cd076185e6ef9916112c24a
Endem1k <vernidub69artem@gmail.com>
2020-06-19 00:46:47 +0300

    aiaia

---
+++ Makoto_Shinkai/js/hj.js
@@ -0,0 +1,357 @@
+let translatePic = function() {
+	let pic = document.getElementById("wrapper_pic_text")
+	let text2 = document.getElementById("five_text_2")
+	let text = document.getElementById("five_text")
+	let text3 = document.getElementById("five_text_3")
+	let text4 = document.getElementById("five_text_4")
+	let hjk = document.getElementById("wrapper_pic_text_2")
+	let ghj = document.getElementById("wrapper_pic_text_3")
+	let yui = document.getElementById("wrapper_pic_text_4")
+	let button = document.getElementById("button")
+	let button2 = document.getElementById("button2")
+	let button3 = document.getElementById("button3")
+	let button4 = document.getElementById("button4")
+	button.style.opacity = "0"
+	button.style.transition = "all .5s ease-in-out"
+	button2.style.opacity = "0"
+	button2.style.transition = "all .5s ease-in-out"
+	button2.style.visibility = "hidden"
+	button3.style.opacity = "0"
+	button3.style.transition = "all .5s ease-in-out"
+	button3.style.visibility = "hidden"
+	button4.style.opacity = "0"
+	button4.style.transition = "all .5s ease-in-out"
+	button4.style.visibility = "hidden"
+	pic.style.width = "91%"
+	pic.style.transition = "all .5s ease-in-out"
+	hjk.style.width = "3%"
+	hjk.style.transition = "all .5s ease-in-out"
+	ghj.style.width = "3%"
+	ghj.style.transition = "all .5s ease-in-out"
+	yui.style.width = "3%"
+	yui.style.transition = "all .5s ease-in-out"
+	text.style.opacity = "1"
+	text.style.transition = " all .3s ease-in-out"
+};
+
+let translatePic_back = function() {
+	let pic = document.getElementById("wrapper_pic_text")
+		text2 = document.getElementById("five_text_2")
+		text = document.getElementById("five_text")
+		text3 = document.getElementById("five_text_3")
+		text4 = document.getElementById("five_text_4")
+		hjk = document.getElementById("wrapper_pic_text_2")
+		ghj = document.getElementById("wrapper_pic_text_3")
+		yui = document.getElementById("wrapper_pic_text_4")
+		button = document.getElementById("button")
+		button2 = document.getElementById("button2")
+		button3 = document.getElementById("button3")
+		button4 = document.getElementById("button4")
+	button.style.opacity = "1"
+	button.style.transition = "all .5s ease-in-out"
+	button.style.visibility = "visible"
+	button2.style.opacity = "1"
+	button2.style.transition = "all .5s ease-in-out"
+	button2.style.visibility = "visible"
+	button3.style.opacity = "1"
+	button3.style.transition = "all .5s ease-in-out"
+	button3.style.visibility = "visible"
+	button4.style.opacity = "1"
+	button4.style.transition = "all .5s ease-in-out"
+	button4.style.visibility = "visible"
+	text.style.opacity = "0"
+	text.style.transition = " all .5s ease-in-out"
+	pic.style.width = "25%"
+	pic.style.transition = "all .5s ease-in-out"
+	hjk.style.width = "25%"
+	hjk.style.transition = "all .5s ease-in-out"
+	ghj.style.width = "25%"
+	ghj.style.transition = "all .5s ease-in-out"
+	yui.style.width = "25%"
+	yui.style.transition = "all .5s ease-in-out"
+	
+};
+
+
+var opacity_back_back = function() {
+	var five_text = document.getElementById('five_text')
+	let pic = document.getElementById("wrapper_pic_text")
+		five_nav = document.getElementById("five-nav")
+		close_opacity = document.getElementById('close_opacity')
+	five_nav.style.width = "50%"
+	five_nav.style.transition = "all .4s ease-in-out"
+	five_text.style.width = "50%"
+	five_text.style.transition = "all .4s ease-in-out"
+	close_opacity.style.opacity = "0"
+	close_opacity.style.transition = "all .3s ease-in-out"
+	close_opacity.style.visibility = "hidden"
+}
+
+var opacity_back = function() {
+	var five_text = document.getElementById('five_text')
+	let pic = document.getElementById("wrapper_pic_text")
+		five_nav = document.getElementById("five-nav")
+		close_opacity = document.getElementById('close_opacity')
+	five_text.style.width = "0%"
+	five_text.style.transition = "all .4s ease-in-out"
+	five_nav.style.width = "100%"
+	five_nav.style.transition = "all .4s ease-in-out"
+	close_opacity.style.opacity = "1"
+	close_opacity.style.transition = "all .5s ease-in-out"
+	close_opacity.style.visibility = "visible"
+}
+
+let translatePic2 = function() {
+	let pic = document.getElementById("wrapper_pic_text")
+		text2 = document.getElementById("five_text_2")
+		text = document.getElementById("five_text")
+		text3 = document.getElementById("five_text_3")
+		text4 = document.getElementById("five_text_4")
+		hjk = document.getElementById("wrapper_pic_text_2")
+		ghj = document.getElementById("wrapper_pic_text_3")
+		yui = document.getElementById("wrapper_pic_text_4")
+		button = document.getElementById("button")
+		button2 = document.getElementById("button2")
+		button3 = document.getElementById("button3")
+		button4 = document.getElementById("button4")
+	button.style.opacity = "0"
+	button.style.transition = "all .5s ease-in-out"
+	button.style.visibility = "hidden"
+	button2.style.opacity = "0"
+	button2.style.transition = "all .5s ease-in-out"
+	button2.style.visibility = "hidden"
+	button3.style.opacity = "0"
+	button3.style.transition = "all .5s ease-in-out"
+	button3.style.visibility = "hidden"
+	button4.style.opacity = "0"
+	button4.style.transition = "all .5s ease-in-out"
+	button4.style.visibility = "hidden"
+	pic.style.width = "3%"
+	pic.style.transition = "all .5s ease-in-out"
+	hjk.style.width = "91%"
+	hjk.style.transition = "all .5s ease-in-out"
+	ghj.style.width = "3%"
+	ghj.style.transition = "all .5s ease-in-out"
+	yui.style.width = "3%"
+	yui.style.transition = "all .5s ease-in-out"
+	text2.style.opacity = "1"
+	text2.style.transition = " all .3s ease-in-out"
+};
+
+let translatePic_back2 = function() {
+	let pic = document.getElementById("wrapper_pic_text")
+	let text2 = document.getElementById("five_text_2")
+	let text = document.getElementById("five_text")
+	let text3 = document.getElementById("five_text_3")
+	let text4 = document.getElementById("five_text_4")
+	let hjk = document.getElementById("wrapper_pic_text_2")
+	let ghj = document.getElementById("wrapper_pic_text_3")
+	let yui = document.getElementById("wrapper_pic_text_4")
+	let button = document.getElementById("button")
+	let button2 = document.getElementById("button2")
+	let button3 = document.getElementById("button3")
+	let button4 = document.getElementById("button4")
+	button.style.opacity = "1"
+	button.style.transition = "all .5s ease-in-out"
+	button.style.visibility = "visible"
+	button2.style.opacity = "1"
+	button2.style.transition = "all .5s ease-in-out"
+	button2.style.visibility = "visible"
+	button3.style.opacity = "1"
+	button3.style.transition = "all .5s ease-in-out"
+	button3.style.visibility = "visible"
+	button4.style.opacity = "1"
+	button4.style.transition = "all .5s ease-in-out"
+	button4.style.visibility = "visible"
+	text2.style.opacity = "0"
+	text2.style.transition = " all .5s ease-in-out"
+	pic.style.width = "25%"
+	pic.style.transition = "all .5s ease-in-out"
+	hjk.style.width = "25%"
+	hjk.style.transition = "all .5s ease-in-out"
+	ghj.style.width = "25%"
+	ghj.style.transition = "all .5s ease-in-out"
+	yui.style.width = "25%"
+	yui.style.transition = "all .5s ease-in-out"
+	
+};
+
+let translatePic3 = function() {
+	let pic = document.getElementById("wrapper_pic_text")
+	let text2 = document.getElementById("five_text_2")
+	let text = document.getElementById("five_text")
+	let text3 = document.getElementById("five_text_3")
+	let text4 = document.getElementById("five_text_4")
+	let hjk = document.getElementById("wrapper_pic_text_2")
+	let ghj = document.getElementById("wrapper_pic_text_3")
+	let yui = document.getElementById("wrapper_pic_text_4")
+	let button = document.getElementById("button")
+	let button2 = document.getElementById("button2")
+	let button3 = document.getElementById("button3")
+	let button4 = document.getElementById("button4")
+	button.style.opacity = "0"
+	button.style.transition = "all .5s ease-in-out"
+	button.style.visibility = "hidden"
+	button2.style.opacity = "0"
+	button2.style.transition = "all .5s ease-in-out"
+	button2.style.visibility = "hidden"
+	button3.style.opacity = "0"
+	button3.style.transition = "all .5s ease-in-out"
+	button3.style.visibility = "hidden"
+	button4.style.opacity = "0"
+	button4.style.transition = "all .5s ease-in-out"
+	button4.style.visibility = "hidden"
+	pic.style.width = "3%"
+	pic.style.transition = "all .5s ease-in-out"
+	hjk.style.width = "3%"
+	hjk.style.transition = "all .5s ease-in-out"
+	ghj.style.width = "91%"
+	ghj.style.transition = "all .5s ease-in-out"
+	yui.style.width = "3%"
+	yui.style.transition = "all .5s ease-in-out"
+	text3.style.opacity = "1"
+	text3.style.transition = " all .3s ease-in-out"
+};
+
+let translatePic_back3 = function() {
+	let pic = document.getElementById("wrapper_pic_text")
+	let text2 = document.getElementById("five_text_2")
+	let text = document.getElementById("five_text")
+	let text3 = document.getElementById("five_text_3")
+	let text4 = document.getElementById("five_text_4")
+	let hjk = document.getElementById("wrapper_pic_text_2")
+	let ghj = document.getElementById("wrapper_pic_text_3")
+	let yui = document.getElementById("wrapper_pic_text_4")
+	let button = document.getElementById("button")
+	let button2 = document.getElementById("button2")
+	let button3 = document.getElementById("button3")
+	let button4 = document.getElementById("button4")
+	button.style.opacity = "1"
+	button.style.transition = "all .5s ease-in-out"
+	button.style.visibility = "visible"
+	button2.style.opacity = "1"
+	button2.style.transition = "all .5s ease-in-out"
+	button2.style.visibility = "visible"
+	button3.style.opacity = "1"
+	button3.style.transition = "all .5s ease-in-out"
+	button3.style.visibility = "visible"
+	button4.style.opacity = "1"
+	button4.style.transition = "all .5s ease-in-out"
+	button4.style.visibility = "visible"
+	text3.style.opacity = "0"
+	text3.style.transition = " all .5s ease-in-out"
+	pic.style.width = "25%"
+	pic.style.transition = "all .5s ease-in-out"
+	hjk.style.width = "25%"
+	hjk.style.transition = "all .5s ease-in-out"
+	ghj.style.width = "25%"
+	ghj.style.transition = "all .5s ease-in-out"
+	yui.style.width = "25%"
+	yui.style.transition = "all .5s ease-in-out"
+	
+};
+
+let translatePic4 = function() {
+	let pic = document.getElementById("wrapper_pic_text")
+	let text2 = document.getElementById("five_text_2")
+	let text = document.getElementById("five_text")
+	let text3 = document.getElementById("five_text_3")
+	let text4 = document.getElementById("five_text_4")
+	let hjk = document.getElementById("wrapper_pic_text_2")
+	let ghj = document.getElementById("wrapper_pic_text_3")
+	let yui = document.getElementById("wrapper_pic_text_4")
+	let button = document.getElementById("button")
+	let button2 = document.getElementById("button2")
+	let button3 = document.getElementById("button3")
+	let button4 = document.getElementById("button4")
+	button.style.opacity = "0"
+	button.style.transition = "all .5s ease-in-out"
+	button.style.visibility = "hidden"
+	button2.style.opacity = "0"
+	button2.style.transition = "all .5s ease-in-out"
+	button2.style.visibility = "hidden"
+	button3.style.opacity = "0"
+	button3.style.transition = "all .5s ease-in-out"
+	button3.style.visibility = "hidden"
+	button4.style.opacity = "0"
+	button4.style.transition = "all .5s ease-in-out"
+	button4.style.visibility = "hidden"
+	pic.style.width = "3%"
+	pic.style.transition = "all .5s ease-in-out"
+	hjk.style.width = "3%"
+	hjk.style.transition = "all .5s ease-in-out"
+	ghj.style.width = "3%"
+	ghj.style.transition = "all .5s ease-in-out"
+	yui.style.width = "91%"
+	yui.style.transition = "all .5s ease-in-out"
+	text4.style.opacity = "1"
+	text4.style.transition = " all .3s ease-in-out"
+};
+
+let translatePic_back4 = function() {
+	let pic = document.getElementById("wrapper_pic_text")
+	let text2 = document.getElementById("five_text_2")
+	let text = document.getElementById("five_text")
+	let text3 = document.getElementById("five_text_3")
+	let text4 = document.getElementById("five_text_4")
+	let hjk = document.getElementById("wrapper_pic_text_2")
+	let ghj = document.getElementById("wrapper_pic_text_3")
+	let yui = document.getElementById("wrapper_pic_text_4")
+	let button = document.getElementById("button")
+	let button2 = document.getElementById("button2")
+	let button3 = document.getElementById("button3")
+	let button4 = document.getElementById("button4")
+	button.style.opacity = "1"
+	button.style.transition = "all .5s ease-in-out"
+	button.style.visibility = "visible"
+	button2.style.opacity = "1"
+	button2.style.transition = "all .5s ease-in-out"
+	button2.style.visibility = "visible"
+	button3.style.opacity = "1"
+	button3.style.transition = "all .5s ease-in-out"
+	button3.style.visibility = "visible"
+	button4.style.opacity = "1"
+	button4.style.transition = "all .5s ease-in-out"
+	button4.style.visibility = "visible"
+	text4.style.opacity = "0"
+	text4.style.transition = " all .5s ease-in-out"
+	pic.style.width = "25%"
+	pic.style.transition = "all .5s ease-in-out"
+	hjk.style.width = "25%"
+	hjk.style.transition = "all .5s ease-in-out"
+	ghj.style.width = "25%"
+	ghj.style.transition = "all .5s ease-in-out"
+	yui.style.width = "25%"
+	yui.style.transition = "all .5s ease-in-out"
+	
+};
+
+let  button = document.getElementById("button")
+button.addEventListener("click", translatePic)
+
+let  button_back = document.getElementById("button_back")
+button_back.addEventListener("click", translatePic_back)
+
+let  button_opacity = document.getElementById("button_opacity")
+button_opacity.addEventListener("click", opacity_back)
+
+let  close_opacity = document.getElementById("close")
+close_opacity.addEventListener("click", opacity_back_back)
+
+let  button2 = document.getElementById("button2")
+button2.addEventListener("click", translatePic2)
+
+let  button_back2 = document.getElementById("button_back_2")
+button_back2.addEventListener("click", translatePic_back2)
+
+let  button3 = document.getElementById("button3")
+button3.addEventListener("click", translatePic3)
+
+let  button_back3 = document.getElementById("button_back_3")
+button_back3.addEventListener("click", translatePic_back3)
+
+let  button4 = document.getElementById("button4")
+button4.addEventListener("click", translatePic4)
+
+let  button_back4 = document.getElementById("button_back_4")
+button_back4.addEventListener("click", translatePic_back4)
